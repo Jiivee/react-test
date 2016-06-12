d000cc9f0ff1a91499afd6b5c4b6bd62ae1cad40
Jiivee <joni.vayrynen@gmail.com>
2016-06-12 12:44:49 +0300

    Show results of last bets

--- src/components/Tournament.js
+++ src/components/Tournament.js
@@ -15,7 +15,8 @@ export default AuthenticatedComponent(class Tournament extends Component {
     this.state = {
       tournament: {},
       points: [],
-      nextbets: []
+      nextbets: [],
+      lastresults: []
     };
   }
 
@@ -23,6 +24,27 @@ export default AuthenticatedComponent(class Tournament extends Component {
     this.fetchTournament();
     this.fetchPoints();
     this.fetchNextBets();
+    this.fetchLastResults();
+  }
+
+  fetchLastResults() {
+    var tournamentId = this.props.params.tournamentId;
+    var jwt = LoginStore.getjwt()
+    var myHeaders = new Headers({
+      'x-access-token': jwt
+    });
+    var path = Constants.MATCH_BETS_URL + 'lastresults/' + tournamentId;
+    fetch(path, {
+      method: 'get',
+      headers: myHeaders
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      this.setState({
+        lastresults: data
+      });
+    })
+    .catch(e => {console.log(e)});
   }
 
   fetchNextBets() {
@@ -31,7 +53,7 @@ export default AuthenticatedComponent(class Tournament extends Component {
     var myHeaders = new Headers({
       'x-access-token': jwt
     });
-    var path = Constants.MATCH_BETS_URL + '/nextbets/' + tournamentId;
+    var path = Constants.MATCH_BETS_URL + 'nextbets/' + tournamentId;
     fetch(path, {
       method: 'get',
       headers: myHeaders
@@ -112,14 +134,13 @@ export default AuthenticatedComponent(class Tournament extends Component {
   render() {
     var tournament = this.state.tournament;
     var nextbets = this.state.nextbets;
+    var lastresults = this.state.lastresults;
     var tournamentId = this.props.params.tournamentId;
     var makeBetsUrl = '/tournaments/' + tournamentId + '/makebets/match';
     var points = this.state.points;
     var userId = LoginStore.getUserId();
     var ownResultsUrl = '/tournaments/' + tournamentId + '/results/' + userId;
     var userEmail = LoginStore.getUser().email;
-
-    //console.log(points);
 
     var user = LoginStore.getUser();
     var adNewUser = '';
@@ -203,6 +224,68 @@ export default AuthenticatedComponent(class Tournament extends Component {
                   {nextbet.map(function(bet) {
                     return (
                       <td key={bet._id}>{bet.score.home}-{bet.score.away}<span className="bet-divider"></span>{bet.mark}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <h3>Results of the last matches</h3>
+        <table>
+          {lastresults.slice(0,1).map(function(nextbet) {
+            return (
+              <thead key="bet-header">
+                <tr>
+                  <td  className="name-col">Name</td>
+                  {nextbet.map(function(bet) {
+                    var key = bet._id + '-head';
+                    var match = bet.match;
+                    return (
+                      <td key={key}><span><div>{match.home_team.short_name}-{match.away_team.short_name}</div><div>{match.score.home}-{match.score.away}</div></span></td>
+                    );
+                  })}
+                </tr>
+              </thead>
+            );
+          })}
+          <tbody>
+            {lastresults.map(function(nextbet) {
+              var userId = nextbet[0].user._id;
+              var urlResults = '/tournaments/' + tournamentId + '/results/' + userId;
+              var key = userId + -'row';
+              return (
+                <tr key={key}>
+                  <td className="name-col"><Link to={urlResults}>{nextbet[0].user.name}</Link></td>
+                  {nextbet.map(function(bet) {
+                    var homeBet = bet.score.home;
+                    var awayBet = bet.score.away;
+                    var markBet = bet.mark;
+                    var homeBetSpan = '';
+                    var awayBetSpan = '';
+                    var markBetSpan = '';
+                    if (bet.score.home === bet.match.score.home) {
+                      homeBetSpan = <span className="bet-right">{homeBet}</span>;
+                    }
+                    else {
+                      homeBetSpan = <span className="bet-wrong">{homeBet}</span>;
+                    }
+
+                    if (bet.score.away === bet.match.score.away) {
+                      awayBetSpan = <span className="bet-right">{awayBet}</span>;
+                    }
+                    else {
+                      awayBetSpan = <span className="bet-wrong">{awayBet}</span>;
+                    }
+
+                    if (bet.mark === bet.match.mark) {
+                      markBetSpan = <span className="bet-right">{markBet}</span>;
+                    }
+                    else {
+                      markBetSpan = <span className="bet-wrong">{markBet}</span>;
+                    }
+                    return (
+                      <td key={bet._id}>{homeBetSpan}-{awayBetSpan}<span className="bet-divider2"></span>{markBetSpan}</td>
                     );
                   })}
                 </tr>
